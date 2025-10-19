49df7b986ea053f893f225b55af1ecb2f531af3d
Isaacpage01-alt <Isaacpage01@hotmail.com>
2025-10-19 16:53:26 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,12 +11,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr">
-      <body
-        className="
-          min-h-screen text-white
-          bg-[url('/bg.jpg?v=5')] bg-fixed bg-cover bg-center
-        "
-      >
+      <body className="min-h-screen text-white bg-[url('/bg.jpg?v=5')] bg-fixed bg-cover bg-center">
         <CartProvider>
           <Header />
           <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-6">
