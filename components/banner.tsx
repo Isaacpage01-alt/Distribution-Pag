2aa8d868eb3c8e662b72b640adc9b1a30a71beb6
Isaacpage01-alt <Isaacpage01@hotmail.com>
2025-10-22 16:31:47 -0400

    Create banner.tsx

---
+++ components/banner.tsx
@@ -0,0 +1,11 @@
+export default function Banner() {
+  return (
+    <div className="w-full">
+      <img
+        src="/banniere.png"
+        alt="Distribution Pagé"
+        className="w-full h-[180px] sm:h-[240px] lg:h-[300px] object-cover"
+      />
+    </div>
+  );
+}
