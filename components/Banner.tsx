7a80eca3d2be5e10c39675b998e4ed3cd19b1120
Isaacpage01-alt <Isaacpage01@hotmail.com>
2025-10-22 16:58:57 -0400

    Update Banner.tsx

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -4,7 +4,14 @@ export default function Banner() {
       <img
         src="/banniere.png"
         alt="Distribution Pagé"
-        className="w-full h-[180px] sm:h-[240px] lg:h-[300px] object-cover"
+        className="
+          w-full h-[180px] sm:h-[240px] lg:h-[300px]
+          object-contain mx-auto
+          mix-blend-multiply   /* << fond blanc 'disparaît' sur le bois */
+          opacity-95           /* ajuste si tu veux plus/moins foncé */
+          pointer-events-none   /* évite surbrillance au clic */
+          select-none
+        "
       />
     </div>
   );
